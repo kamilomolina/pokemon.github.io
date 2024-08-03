d4567b1ef6c7127d02ad1fbfaf1c68544be73b07
kamilomolina <134764828+kamilomolina@users.noreply.github.com>
2024-08-03 10:36:29 -0600

    una kk

--- PROYECTO/backend/routes/pokemonRoutes.js
+++
@@ -1,9 +0,0 @@
-// backend/routes/PokemonRoutes.js
-const express = require('express');
-const router = express.Router();
-const pokemonController = require('../controllers/pokemonController');
-
-router.get('/', pokemonController.getAllPokemon);
-router.post('/', pokemonController.createPokemon);
-
-module.exports = router;
